0c01d0f460898c9da2e999b5e736943319020cfc
Ay-iman <ayomiderabiu00@gmail.com>
2023-03-07 20:17:41 +0000

    making changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for i in range(0, 100):
+    if i != 99:
+        var = ", "
+    else:
+        var = "\n"
+    if i < 10:
+        print("{}".format(0), end="")
+    print("{}".format(i), end=var)
